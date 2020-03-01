a0e820db4da5d7d6ecb969fcffb978d96b808ac7
Charles-Antoine Mathieu <ca.mathieu@criteo.com>
2020-03-01 21:07:02 +0100

    Refacto 1.3-RC1

---
+++ testing/postgres/run.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+set -e
+cd "$(dirname "$0")"
+
+BACKEND="postgres"
+CMD=$1
+TEST=$2
+
+source ../utils.sh
+check_docker_connectivity
+
+DOCKER_IMAGE="postgres:latest"
+DOCKER_NAME="plik.postgres"
+DOCKER_PORT=2602
+PASSWORD="password"
+
+function start {
+    if status ; then
+        echo "ALREADY RUNNING"
+    else
+        echo -e "\n - Pulling $DOCKER_IMAGE\n"
+        docker pull "$DOCKER_IMAGE"
+        if docker ps -a -f name="$DOCKER_NAME" | grep "$DOCKER_NAME" > /dev/null ; then
+            docker rm -f "$DOCKER_NAME"
+        fi
+
+        echo -e "\n - Starting $DOCKER_NAME\n"
+        docker run -d -p "$DOCKER_PORT:5432" -e POSTGRES_PASSWORD="$PASSWORD" --name "$DOCKER_NAME" "$DOCKER_IMAGE"
+
+        sleep 1
+        if ! status ; then
+            echo "IMAGE IS NOT RUNNING"
+            exit 1
+        fi
+    fi
+}
+
+function stop {
+    if status ; then
+        echo -e "\n - Removing $DOCKER_NAME\n"
+        docker rm -f "$DOCKER_NAME" >/dev/null
+    else
+        echo "NOT RUNNING"
+    fi
+}
+
+function status {
+    docker ps -f name="$DOCKER_NAME" | grep "$DOCKER_NAME" > /dev/null
+}
+
+case "$CMD" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  test)
+    stop
+    start
+    run_tests "$BACKEND" "$TEST"
+    ;;
+  status)
+    if status ; then
+        docker ps -f name="$DOCKER_NAME"
+    else
+        echo "NOT RUNNING"
+    fi
+    ;;
+  *)
+	echo "Usage: $0 {start|stop|restart|status}"
+	exit 1
+esac
+
+exit 0
